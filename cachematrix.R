dc20ad3ca311d09f2a15a37b463789e983885ad5
Mohamad EL-Husseini <Mohamad EL-Husseini>
2016-01-24 14:12:36 -0700

    Fixed the indentation in the code

--- cachematrix.R
+++ cachematrix.R
@@ -6,23 +6,23 @@
 ## can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-## initialize the value of the matrix inverse to NULL
-matinverse <- NULL
-## Defining function set where the value be cached in
-set <- function(y) {
-  x <<- y ## caches the inputted matrix
-  matinverse <<- NULL ## sets the value of the matrix inverse to NULL
-}
-## get function returns  the inputted matrix
-get <- function() x
-## setinverse function set the inverse of the inputted matrix
-setinverse <- function(solve) matinverse <<- solve
-## getinverse function returns the inverse of the inputted matrix
-getinverse <- function() matinverse
-## returns the above functions for makeCacheMatrix
-list(set = set, get = get, 
-      setinverse = setinverse, 
-      getinverse = getinverse)
+    ## initialize the value of the matrix inverse to NULL
+    matinverse <- NULL
+    ## Defining function set where the value be cached in
+    set <- function(y) {
+      x <<- y ## caches the inputted matrix
+      matinverse <<- NULL ## sets the value of the matrix inverse to NULL
+    }
+    ## get function returns  the inputted matrix
+    get <- function() x
+    ## setinverse function set the inverse of the inputted matrix
+    setinverse <- function(solve) matinverse <<- solve
+    ## getinverse function returns the inverse of the inputted matrix
+    getinverse <- function() matinverse
+    ## returns the above functions for makeCacheMatrix
+    list(set = set, get = get, 
+          setinverse = setinverse, 
+          getinverse = getinverse)
 
 }
 
@@ -33,23 +33,23 @@ list(set = set, get = get,
 #function below will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
- 
-## declared minverse variable 
-  minverse <- x$getinverse()
-## if the inverse is already in cache and the matrix has not 
-## changed it gets it
-  if (!is.null(minverse) & identical(minverse,solve(x$get(), ...))) {
-    message("getting cached matrix")
-    return(minverse) 
-  }
-
-## if the inverse is not in cache, it calculates it
-## then sets it and return the inverse.
-    data <- x$get()
-    x$set(data)
-    minverse <- solve(data, ...)
-    x$setinverse(minverse)
-    minverse
+    ## Return a matrix that is the inverse of 'x'
+     
+    ## declared minverse variable 
+      minverse <- x$getinverse()
+    ## if the inverse is already in cache and the matrix has not 
+    ## changed it gets it
+      if (!is.null(minverse) & identical(minverse,solve(x$get(), ...))) {
+        message("getting cached matrix")
+        return(minverse) 
+      }
+    
+    ## if the inverse is not in cache, it calculates it
+    ## then sets it and return the inverse.
+        data <- x$get()
+        x$set(data)
+        minverse <- solve(data, ...)
+        x$setinverse(minverse)
+        minverse
   
 }
